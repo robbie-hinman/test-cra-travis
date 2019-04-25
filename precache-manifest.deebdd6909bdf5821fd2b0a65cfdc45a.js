972c79c0a2149b313bc9a25f580b0928963a1b5f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-25 22:26:02 +0000

    Deploy robbie-hinman/test-cra-travis to github.com/robbie-hinman/test-cra-travis.git:gh-pages

---
+++ precache-manifest.deebdd6909bdf5821fd2b0a65cfdc45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3328f93a2f0cb70d17c15a66254145",
+    "url": "/test-cra-travis/index.html"
+  },
+  {
+    "revision": "5d482e94629b3a5a3504",
+    "url": "/test-cra-travis/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/test-cra-travis/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "5d482e94629b3a5a3504",
+    "url": "/test-cra-travis/static/js/main.68bdac3d.chunk.js"
+  },
+  {
+    "revision": "ce57a0b40f7d3b791207",
+    "url": "/test-cra-travis/static/js/runtime~main.63900fc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-cra-travis/static/media/logo.5d5d9eef.svg"
+  }
+]);
